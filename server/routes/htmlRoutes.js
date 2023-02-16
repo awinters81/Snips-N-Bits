f40ae136c942b6625ba17fe8b65bd6afc3033e71
awinters81 <alexandria.winters81@gmail.com>
2023-02-16 07:28:19 -0500

    created dist folder and started on html file

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+// So i have to create a dist folder with an html file...
+
 module.exports = (app) =>
   app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../client/dist/index.html'))
